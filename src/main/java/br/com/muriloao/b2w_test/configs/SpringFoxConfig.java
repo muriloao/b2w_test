7dc8cfaaefdd0a71c46728f2ddfefb5c5a90fa6b
Murilo Oliveira <murilo.ao@hotmail.com>
2020-08-07 02:04:45 -0300

    docs(swagger): Adding Swagger

---
+++ src/main/java/br/com/muriloao/b2w_test/configs/SpringFoxConfig.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.muriloao.b2w_test.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ *
+ * @author Murilo Oliveira
+ */
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("B2W Test")
+                .description("implementation of an API in Spring to solve the B2W test")
+                .version("1.0.0")
+                .contact(new Contact("Murilo Oliveira", "", "murilo.ao@hotmail.com"))
+                .build();
+    }
+
+}
