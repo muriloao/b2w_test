5a33a1c8e884f534397810b9c7055c8e2935c826
Murilo Oliveira <murilo.ao@hotmail.com>
2020-08-06 23:56:07 -0300

    chore(app): Initial API

---
+++ src/main/java/br/com/muriloao/b2w_test/B2wTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.muriloao.b2w_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2wTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(B2wTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/muriloao/b2w_test/B2wTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.muriloao.b2w_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2wTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
